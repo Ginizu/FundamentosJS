593cdc78fddfeb2063a9e2f27754522b9af2527b
Ginizu <diniztorres.giovanna@gmail.com>
2025-06-19 22:14:42 -0300

    Projetinhos cadastro de recrutas

---
+++ Cadastro/index.js
@@ -0,0 +1,10 @@
+let name = window.prompt("Insira seu nome:")
+let lastname = window.prompt("Insira seu sobrenome:")
+let area = prompt("Qual a area de atuação?")
+let data = prompt("Qual o ano de nascimento?")
+
+parseFloat(data)
+
+year = 2025 - data
+
+console.log("Seu nome é", name, lastname, "seu campo é", area, "sua idade é", year )
