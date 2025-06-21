efe7bd2deae568a8aa1a359f23de551d1ebe5edb
Ginizu <diniztorres.giovanna@gmail.com>
2025-06-21 20:20:06 -0300

    Exercicios: Cadastro de usuarios e calculadora

--- Cadastro/index.js
+++ Cadastro/index.js
@@ -3,8 +3,6 @@ let lastname = window.prompt("Insira seu sobrenome:")
 let area = prompt("Qual a area de atuação?")
 let data = prompt("Qual o ano de nascimento?")
 
-parseFloat(data)
-
-year = 2025 - data
-
-console.log("Seu nome é", name, lastname, "seu campo é", area, "sua idade é", year )
+alert("Seu nome é" + name + " " + lastname + "seu campo é" 
+    + area + "sua idade é" + (2025 - data) 
+)
